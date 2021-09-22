7e87e7db225316e05016dd3027dc7590f24b0b03
unknown <pires00243@gmail.com>
2021-09-22 19:46:10 +0100

    Writing JUnit5 unit tests correction final

--- src/test/java/pira_exercice/java_Junit01/DataServiceTest.java
+++ src/test/java/pira_exercice/java_Junit01/DataServiceTest.java
@@ -58,7 +58,10 @@ class DataServiceTest {
         TolkienCharacter tolkienCharacter = dataService.getFellowship().get(0);
         // TODO check that tolkienCharacter.getClass is not a movie class
        
-        assertNotEquals(tolkienCharacter, Movie.class);
+        assertNotEquals(tolkienCharacter, Movie.class); // what i do
+        
+        assertFalse(Movie.class.isAssignableFrom(tolkienCharacter.getClass())); // what is correct 
+        assertTrue(TolkienCharacter.class.isAssignableFrom(tolkienCharacter.getClass())); // what is correct
         
     }
 
@@ -86,6 +89,14 @@ class DataServiceTest {
 
         // TODO check that Frodo and Gandalf are part of the fellowship
         
+        
+        //Correction 
+        
+        TolkienCharacter frodo = new TolkienCharacter("Frodo", 33, Race.HOBBIT);
+        TolkienCharacter gandalf = new TolkienCharacter("Gandalf", 2020, Race.MAIA);
+
+        assertTrue(fellowship.contains(frodo));
+        assertTrue(fellowship.contains(gandalf));
     
     }
 
@@ -97,6 +108,10 @@ class DataServiceTest {
         
         // TODO test that at least one ring bearer is part of the fellowship
         
+        //correction
+        
+        Map<Ring, TolkienCharacter> ringBearers = dataService.getRingBearers();
+        assertTrue(ringBearers.values().stream().anyMatch(ringBearer -> fellowship.contains(ringBearer)));
          
         
       
@@ -118,7 +133,19 @@ class DataServiceTest {
 
         // ensure that the order of the fellowship is:
         // frodo, sam, merry,pippin, gandalf,legolas,gimli,aragorn,boromir
-        fail("not yet implemented");
+
+        //correction
+        
+        assertEquals(dataService.getFellowshipCharacter("Frodo"), fellowship.get(0));
+        assertEquals(dataService.getFellowshipCharacter("Sam"), fellowship.get(1));
+        assertEquals(dataService.getFellowshipCharacter("Merry"), fellowship.get(2));
+        assertEquals(dataService.getFellowshipCharacter("Pippin"), fellowship.get(3));
+        assertEquals(dataService.getFellowshipCharacter("Gandalf"), fellowship.get(4));
+        assertEquals(dataService.getFellowshipCharacter("Legolas"), fellowship.get(5));
+        assertEquals(dataService.getFellowshipCharacter("Gimli"), fellowship.get(6));
+        assertEquals(dataService.getFellowshipCharacter("Aragorn"), fellowship.get(7));
+        assertEquals(dataService.getFellowshipCharacter("Boromir"), fellowship.get(8));
+
     }
 
     @Test
@@ -128,7 +155,18 @@ class DataServiceTest {
         // TODO test ensure that all hobbits and men are younger than 100 years
 
         // TODO also ensure that the elfs, dwars the maia are all older than 100 years
-        fail("not yet implemented");
+       
+        //correction
+        
+        assertTrue(fellowship.stream()
+                .filter(fellow -> fellow.getRace().equals(Race.HOBBIT) || fellow.getRace().equals(Race.MAN))
+                .allMatch(fellow -> fellow.age < 100));
+
+        assertTrue(
+                fellowship
+                        .stream().filter(fellow -> fellow.getRace().equals(Race.ELF)
+                                || fellow.getRace().equals(Race.DWARF) || fellow.getRace().equals(Race.MAIA))
+                        .allMatch(fellow -> fellow.age > 100));
 
         // HINT fellowship.stream might be useful here
         
