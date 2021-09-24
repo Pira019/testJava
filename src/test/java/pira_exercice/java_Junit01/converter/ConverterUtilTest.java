5f981cb09b09dbe0cb736a96c9113490590a5aa8
unknown <pires00243@gmail.com>
2021-09-24 17:32:49 +0100

    Write a dynamic test

---
+++ src/test/java/pira_exercice/java_Junit01/converter/ConverterUtilTest.java
@@ -0,0 +1,19 @@
+package pira_exercice.java_Junit01.converter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConverterUtilTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
