16b312fa2e5863f6f63d3846cef12b9861a0bdf6
unknown <pires00243@gmail.com>
2021-09-27 12:31:22 +0100

    dynamic test

--- src/test/java/pira_exercice/java_Junit01/converter/ConverterUtilTest.java
+++ src/test/java/pira_exercice/java_Junit01/converter/ConverterUtilTest.java
@@ -1,19 +1,49 @@
 package pira_exercice.java_Junit01.converter;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class ConverterUtilTest {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+	 int[][] celsiusFahrenheitMapping = new int[][] { { 10, 50 }, { 40, 104 }, { 0, 32 } };
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	    @TestFactory
+	    Stream<DynamicTest> ensureThatCelsiumConvertsToFahrenheit() {
+	    	
+	        return Arrays.stream(celsiusFahrenheitMapping).map(entry -> {
+	            // access celcius and fahrenheit from entry
+	            int celsius = entry[0];
+	            int fahrenheit = entry[1];
+	           
+	            return dynamicTest(celsius +"  Celsius are  "+ fahrenheit, () -> { assertEquals(fahrenheit,ConverterUtil.convertCelsiusToFahrenheit(celsius));});
+	         //   return null;
+	            // return a dynamicTest which checks that that the convertion from celcius to
+	            // fahrenheit is correct
+	        });
+
+	    }
+
+	    @TestFactory
+	     public Stream<DynamicTest> ensureThatFahrenheitConvertsToCelsius() {
+	    	
+	    	   // TODO Write a similar test fahrenheit to celsius
+	         ConverterUtil converter = new ConverterUtil();
+	            int[][] data = new int[][] { { 10,50 }, { 40, 104}, { 0, 32 } };
+	            return Arrays.stream(data).map(entry -> {
+	                int celsius = entry[0];
+	                int fahrenheit = entry[1];
+	                return dynamicTest(celsius + " Celsius are " + fahrenheit, () -> {
+	                    assertEquals(celsius, converter.convertFahrenheitToCelsius(fahrenheit));
+	                });
+	            });
+	        }
 
 }
